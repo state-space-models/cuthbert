a94126c387ed6d09cd44af194bf09d03f94012e0
Adrien Corenflos <adrien.corenflos@gmail.com>
2024-11-06 15:21:46 +0000

    Reinstate the platform switch and tests

--- resampling/utils.py
+++ resampling/utils.py
@@ -1,21 +1,65 @@
+from functools import partial
+
+import jax.lax
+import numpy as np
 from jax import Array, numpy as jnp
 from jax.typing import ArrayLike
 from jax.scipy.special import logsumexp
 
+import numba as nb
+from jax.lax import platform_dependent
 
-def inverse_cdf(sorted_uniforms: ArrayLike, logits: ArrayLike) -> Array:
+
+@jax.jit
+def inverse_cdf(sorted_uniforms: ArrayLike, logits: ArrayLike) -> ArrayLike:
     """
     Inverse CDF sampling for resampling algorithms.
 
     Args:
         sorted_uniforms: Sorted uniforms.
-        logits: Log-weights, possibly unnormalized.
+        logits: Log-weights, possibly un-normalized.
 
     Returns:
         Indices of the particles to be resampled.
     """
     weights = jnp.exp(logits - logsumexp(logits))
+    return platform_dependent(sorted_uniforms, weights, cpu=inverse_cdf_cpu, default=inverse_cdf_default)
+
+
+@jax.jit
+def inverse_cdf_default(sorted_uniforms: jnp.ndarray, weights: jnp.ndarray) -> jnp.ndarray:
     M = weights.shape[0]
     cs = jnp.cumsum(weights)
     idx = jnp.searchsorted(cs, sorted_uniforms, method="sort")
     return jnp.clip(idx, 0, M - 1)
+
+
+@jax.jit
+def inverse_cdf_cpu(sorted_uniforms: jnp.ndarray, weights: jnp.ndarray):
+    M = weights.shape[0]
+    N = sorted_uniforms.shape[0]
+    idx = jnp.zeros(N, dtype=int)
+
+    def callback(args):
+        idx_, su, w = args
+        idx_ = np.copy(idx_)
+        su = np.asarray(su)
+        w = np.asarray(w)
+        inverse_cdf_numba(idx_, su, w)
+        return idx_
+
+    idx = jax.pure_callback(callback, idx, (idx, sorted_uniforms, weights), vmap_method="sequential")
+    return jnp.clip(idx, 0, M - 1)
+
+
+@nb.njit(boundscheck=False, nogil=True)
+def inverse_cdf_numba(idx, su, ws):
+    j = 0
+    s = ws[0]
+    M = su.shape[0]
+
+    for n in range(M):
+        while su[n] > s:
+            j += 1
+            s += ws[j]
+        idx[n] = j
---
+++ tests/resampling/utils.py
@@ -0,0 +1,44 @@
+import chex
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax.scipy.special import logsumexp
+
+
+def resampling_tester(rng_key, log_weights, resampling, m, k):
+    keys = jax.random.split(rng_key, k)
+    indices = jax.vmap(resampling, [0, None])(keys, log_weights)
+    _check_bincounts(indices, log_weights, m, k)
+
+
+def conditional_resampling_tester(rng_key, log_weights, resampling, conditional_resampling, m, k):
+
+    def do_one(key):
+        # check that Bayes rule is satisfied:
+        # we sample the index of the pivot particle unconditionally,
+        # and then the rest conditionally and then return the indices, which
+        # should be the same as the unconditional resampling
+
+        key_i, key_resampling, key_conditional = jax.random.split(key, 3)
+        pivot_in = jax.random.randint(key_i, (), 0, m)
+
+        unconditional_indices = resampling(key_resampling, log_weights)
+        pivot_out = unconditional_indices[pivot_in]
+
+        conditional_indices = conditional_resampling(key_conditional, log_weights, pivot_in, pivot_out)
+        return conditional_indices, pivot_out, conditional_indices[pivot_in]
+
+    keys = jax.random.split(rng_key, k)
+    indices, expected_pivots_out, pivots_out = jax.vmap(do_one)(keys)
+
+    chex.assert_trees_all_equal(expected_pivots_out, pivots_out)
+    _check_bincounts(indices, log_weights, m, k)
+
+
+def _check_bincounts(indices, log_weights, m, k):
+    n = log_weights.shape[0]
+    counts = jax.vmap(lambda z: jnp.bincount(z, length=n))(indices)
+    probs = jnp.mean(counts, axis=0) / m
+    expected_probs = jnp.exp(log_weights - logsumexp(log_weights))
+    tol = 2 * m / np.sqrt(k)
+    chex.assert_trees_all_close(probs, expected_probs, rtol=tol, atol=tol)
