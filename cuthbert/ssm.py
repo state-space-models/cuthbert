ea78488af9d6fccc9a59c5e04f7682b24111e6ce
Sam Duffield <s@mduffield.com>
2025-01-23 11:58:32 +0000

    Generalize to FK

--- cuthbert/ssm.py
+++ cuthbert/ssm.py
@@ -10,61 +10,48 @@ from cuthbert.types import (
 class InitLogDensity(Protocol):
     def __call__(
         self,
-        u: ArrayTreeLike,
         x: ArrayTreeLike,
+        u: ArrayTreeLike,
     ) -> ScalarArray: ...
 
 
 class InitSample(Protocol):
     def __call__(
         self,
-        u: ArrayTreeLike,
         key: KeyArray,
+        u: ArrayTreeLike,
     ) -> ArrayTree: ...
 
 
 class DynamicsLogDensity(Protocol):
     def __call__(
         self,
-        x_prev: ArrayTreeLike,
-        u: ArrayTreeLike,
         x: ArrayTreeLike,
+        x_traj: ArrayTreeLike,
+        u: ArrayTreeLike,
     ) -> ScalarArray: ...
 
 
 class DynamicsSample(Protocol):
     def __call__(
         self,
-        x_prev: ArrayTreeLike,
-        u: ArrayTreeLike,
         key: KeyArray,
+        x_traj: ArrayTreeLike,
+        u: ArrayTreeLike,
     ) -> ArrayTree: ...
 
 
-class ObservationLogDensity(Protocol):
+class LogLikelihood(Protocol):
     def __call__(
         self,
+        x_traj: ArrayTreeLike,
         u: ArrayTreeLike,
-        x: ArrayTreeLike,
-        y: ArrayTreeLike,
     ) -> ScalarArray: ...
 
 
-class ObservationSample(Protocol):
-    def __call__(
-        self,
-        u: ArrayTreeLike,
-        x: ArrayTreeLike,
-        key: KeyArray,
-    ) -> ArrayTree: ...
-
-
-class StateSpaceModel(
-    NamedTuple
-):  # Not all inference methods will require all components, if not required set to RaiseNotImplementedError function
+class FeynmanKac(NamedTuple):
     init_log_density: InitLogDensity
     init_sample: InitSample
     dynamics_log_density: DynamicsLogDensity
     dynamics_sample: DynamicsSample
-    observation_log_density: ObservationLogDensity
-    observation_sample: ObservationSample
+    log_likelihood: LogLikelihood
