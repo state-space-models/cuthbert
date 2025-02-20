153bff58b296234d453dd28b15eeec9194c0f848
Sam Duffield <s@mduffield.com>
2025-02-20 11:23:42 +0000

    Add init

---
+++ kalman/__init__.py
@@ -0,0 +1,7 @@
+from kalman.filter import offline_filter
+from kalman.filter import KalmanState
+from kalman.filter import KalmanFilterInfo
+from kalman.smoother import smoother
+from kalman.smoother import KalmanSmootherInfo
+from kalman.online import predict
+from kalman.online import update
