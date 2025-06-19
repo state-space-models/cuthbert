10578ea836fa2d6f339e75e019681ca66121b616
Sahel Iqbal <sahel13miqbal@proton.me>
2025-06-19 22:29:14 +0300

    Rename Protocols and add smoothing skeletons

--- cuthbert/smc/bootstrap.py
+++ cuthbert/smc/bootstrap.py
@@ -8,30 +8,33 @@ from cuthbertlib.resampling import Resampling
 from cuthbertlib.types import ArrayTree, ArrayTreeLike, KeyArray, ScalarArray
 
 
-class M0(Protocol):
-    """Get a sample from the initial distribution $M_0(x_0)$."""
+class InitSample(Protocol):
+    """Get a sample from the initial distribution :math:`M_0(x_0)`."""
 
     def __call__(self, key: KeyArray, model_inputs: ArrayTreeLike) -> ArrayTree: ...
 
 
-class Mt(Protocol):
-    """Sample from the Markov kernel $M_t(x_t \\mid x_{t-1})$."""
+class PropagateSample(Protocol):
+    """Sample from the Markov kernel :math:`M_t(x_t \\mid x_{t-1})`."""
 
     def __call__(
         self, key: KeyArray, state: ArrayTreeLike, model_inputs: ArrayTreeLike
     ) -> ArrayTree: ...
 
 
-class LogG0(Protocol):
-    """Compute the log potential function $\\log G_0(x_0)$."""
+class TransitionDensity(Protocol):
+    """Compute the transition density :math:`\\log M_t(x_t \\mid x_{t-1})`."""
 
     def __call__(
-        self, state: ArrayTreeLike, model_inputs: ArrayTreeLike
+        self,
+        state_prev: ArrayTreeLike,
+        state: ArrayTreeLike,
+        model_inputs: ArrayTreeLike,
     ) -> ScalarArray: ...
 
 
-class LogGt(Protocol):
-    """Compute the log potential function $\\log G_t(x_{t-1}, x_t)$."""
+class LogPotential(Protocol):
+    """Compute the log potential function :math:`\\log G_t(x_{t-1}, x_t)`."""
 
     def __call__(
         self,
@@ -43,73 +46,89 @@ class LogGt(Protocol):
 
 class BootstrapFilterState(NamedTuple):
     key: KeyArray
-    particles: ArrayTree | None
-    log_weights: Array | None
-    ancestor_indices: Array | None
+    particles: ArrayTree
+    log_weights: Array
+    ancestor_indices: Array
     model_inputs: ArrayTreeLike
-    log_likelihood_incr: ScalarArray | None
+    log_likelihood: ScalarArray
+
+
+class SmootherState(NamedTuple):
+    particles: ArrayTree
+    filtered_particles: ArrayTree
+    filtered_log_weights: Array
 
 
 def init_prepare(
     model_inputs: ArrayTreeLike,
-    m0: M0,
-    log_g0: LogG0,
+    init_sample: InitSample,
     num_samples: int,
     key: KeyArray,
 ) -> BootstrapFilterState:
     keys = random.split(key, num_samples)
-    particles = jax.vmap(m0, (0, None))(keys, model_inputs)
-    log_weights = jax.vmap(log_g0, (0, None))(particles, model_inputs)
+    particles = jax.vmap(init_sample, (0, None))(keys, model_inputs)
     return BootstrapFilterState(
-        key, particles, log_weights, jnp.array(0.0), model_inputs, None
+        key=key,
+        particles=particles,
+        log_weights=jnp.zeros(num_samples),
+        ancestor_indices=jnp.arange(num_samples),
+        model_inputs=model_inputs,
+        log_likelihood=jnp.array(0.0),
     )
 
 
 def filter_prepare(
     model_inputs: ArrayTreeLike,
+    init_sample: InitSample,
     num_samples: int,
     key: KeyArray,
 ) -> BootstrapFilterState:
-    key, sub_key = random.split(key)
-    return BootstrapFilterState(key, None, None, None, model_inputs, None)
+    dummy_particle = jax.eval_shape(init_sample, key, model_inputs)
+    particles = tree.map(lambda x: jnp.empty((num_samples,) + x.shape), dummy_particle)
+    return BootstrapFilterState(
+        key=key,
+        particles=particles,
+        log_weights=jnp.zeros(num_samples),
+        ancestor_indices=jnp.arange(num_samples),
+        model_inputs=model_inputs,
+        log_likelihood=jnp.array(0.0),
+    )
 
 
 def filter_combine(
     state_1: BootstrapFilterState,
     state_2: BootstrapFilterState,
-    mt: Mt,
-    log_gt: LogGt,
+    propagate_sample: PropagateSample,
+    log_potential: LogPotential,
     resampling_fn: Resampling,
     ess_threshold: float,
 ) -> BootstrapFilterState:
-    if state_1.particles is None or state_1.log_weights is None:
-        raise ValueError("state_1 must have particles and log_weights")
-
     N = state_1.particles.shape[0]
     keys = random.split(state_1.key, N + 1)
 
     # Resample
     ancestor_indices, log_weights = jax.lax.cond(
         log_ess(state_1.log_weights) < jnp.log(ess_threshold),
-        lambda: (resampling_fn(keys[0], state_1.log_weights, N), jnp.zeros(N)),  # type: ignore
+        lambda: (resampling_fn(keys[0], state_1.log_weights, N), jnp.zeros(N)),
         lambda: (jnp.arange(N), state_1.log_weights),
     )
     ancestors = tree.map(lambda x: x[ancestor_indices], state_1.particles)
 
-    # Move
-    next_particles = jax.vmap(mt, (0, 0, None))(
+    # Propagate
+    next_particles = jax.vmap(propagate_sample, (0, 0, None))(
         keys[1:], ancestors, state_1.model_inputs
     )
 
     # Reweight
-    log_potentials = jax.vmap(log_gt, (0, 0, None))(
+    log_potentials = jax.vmap(log_potential, (0, 0, None))(
         ancestors, next_particles, state_1.model_inputs
     )
     next_log_weights = log_potentials + log_weights
 
-    # Compute the log likelihood increment
+    # Compute the log likelihood
     logsum_weights = jax.nn.logsumexp(next_log_weights)
     log_likelihood_incr = logsum_weights - jax.nn.logsumexp(log_weights)
+    log_likelihood = log_likelihood_incr + state_1.log_likelihood
 
     return BootstrapFilterState(
         state_2.key,
@@ -117,9 +136,60 @@ def filter_combine(
         next_log_weights,
         ancestor_indices,
         state_2.model_inputs,
-        log_likelihood_incr,
+        log_likelihood,
     )
 
 
 def log_ess(log_weights: Array) -> Array:
+    """Logarithm of the effective sample size."""
     return 2 * jax.nn.logsumexp(log_weights) - jax.nn.logsumexp(2 * log_weights)
+
+
+def convert_filter_to_smoother_state(
+    filter_state: BootstrapFilterState,
+    n_smoother_particles: int,
+    key: KeyArray,
+) -> SmootherState:
+    weights = jax.nn.softmax(filter_state.log_weights)
+    indices = random.choice(
+        key, filter_state.particles.shape[0], (n_smoother_particles,), p=weights
+    )
+    return SmootherState(
+        filter_state.particles[indices],
+        filter_state.particles,
+        filter_state.log_weights,
+    )
+
+
+def smoother_prepare(
+    filter_state: BootstrapFilterState,
+    model_inputs: ArrayTreeLike,
+    n_smoother_particles: int,
+    key: KeyArray,
+) -> SmootherState:
+    dummy_smoothed_particles = tree.map(
+        lambda x: jnp.empty((n_smoother_particles,) + x.shape[1:]),
+        filter_state.particles,
+    )
+    return SmootherState(
+        dummy_smoothed_particles,
+        filter_state.particles,
+        filter_state.log_weights,
+    )
+
+
+def smoother_combine(
+    state_1: SmootherState,
+    state_2: SmootherState,
+    transition_density: TransitionDensity,
+    key: KeyArray,
+) -> SmootherState:
+    """Backward sampling.
+
+    Args:
+        state_1: Output of `smoother_prepare` at time t.
+        state_2: Smoother state at time t + 1.
+        transition_density: The state transition density function.
+        key: JAX random key.
+    """
+    ...
