9b4a8edf66667d8a9736f733685a113780b4658c
Adrien Corenflos <adrien.corenflos@gmail.com>
2025-06-30 11:05:21 +1000

    Some pyright should be fixed, but not working on my laptop

--- cuthbertlib/smc/smoothing/mcmc.py
+++ cuthbertlib/smc/smoothing/mcmc.py
@@ -40,7 +40,7 @@ def simulate(
         https://arxiv.org/abs/2207.00976
     """
     init_key, mcmc_key = jax.random.split(key, 2)
-    n_samples = log_weight_x0_all.shape[0]
+    n_samples = x1_ancestors.shape[0]
 
     keys = random.split(mcmc_key, n_steps)
 
@@ -59,11 +59,11 @@ def simulate(
         acc = lu < log_alpha
 
         idx = jnp.where(acc, prop_idx, idx)
-        x0_res = x0_all[idx]
+        x0_res = jax.tree_map(lambda z: z[idx], x0_all)
         idx_log_p = jnp.where(acc, prop_log_p, idx_log_p)
         return (idx, x0_res, idx_log_p), None
 
-    x0_init = x0_all[x1_ancestors]
+    x0_init = jax.tree_map(lambda z: z[x1_ancestors], x0_all)
     init_log_p = jax.vmap(log_density)(x1_all, x0_init)
     init = (x1_ancestors, x0_init, init_log_p)
     (out_index, out_samples, _), _ = jax.lax.scan(body, init, keys)
