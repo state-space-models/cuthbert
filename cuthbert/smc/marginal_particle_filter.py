Fix MPF normalization

cuthbert/smc/marginal_particle_filter.py
@@ -185,7 +185,9 @@ def filter_combine(
     keys = random.split(state_1.key, N + 1)
 
     # Resample
-    prev_log_weights = state_1.log_weights
+    prev_log_weights = state_1.log_weights - jax.nn.logsumexp(
+        state_1.log_weights
+    )  # Ensure normalized
     ancestor_indices, log_weights = jax.lax.cond(
         log_ess(state_1.log_weights) < jnp.log(ess_threshold * N),
         lambda: (resampling_fn(keys[0], state_1.log_weights, N), jnp.zeros(N)),

tests/cuthbert/smc/test_particle_filters.py
@@ -1,5 +1,4 @@
 from functools import partial
-
 import chex
 import jax
 import jax.numpy as jnp
@@ -95,10 +94,9 @@ class Test(chex.TestCase):
             m0, chol_P0, Fs, cs, chol_Qs, Hs, ds, chol_Rs, ys, method
         )
         key = random.key(seed + 1)
-        states = self.variant(filter, static_argnames=("inference", "parallel"))(
+        states = self.variant(filter, static_argnames=("filter_obj", "parallel"))(
             inference, model_inputs, parallel=False, key=key
         )
-        states = filter(inference, model_inputs, parallel=False, key=key)
         weights = jax.nn.softmax(states.log_weights)
         means = jnp.sum(states.particles * weights[..., None], axis=1)
         covs = jax.vmap(lambda particles, w: jnp.cov(particles.T, aweights=w))(
@@ -188,7 +186,7 @@ class Test(chex.TestCase):
         # Run the particle filter
         model_inputs = jnp.empty(num_time_steps + 1)
         key, subkey = random.split(key)
-        states = self.variant(filter, static_argnames=("inference", "parallel"))(
+        states = self.variant(filter, static_argnames=("filter_obj", "parallel"))(
             inference, model_inputs, parallel=False, key=subkey
         )
 