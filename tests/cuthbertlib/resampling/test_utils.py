31036b7693537e46ee23d502fc32ff69b850f008
Sam Duffield <s@mduffield.com>
2025-10-21 17:30:02 +0100

    Serialize test inverse_cdf_cpu

--- tests/cuthbertlib/resampling/test_utils.py
+++ tests/cuthbertlib/resampling/test_utils.py
@@ -4,6 +4,7 @@ import chex
 import jax
 import jax.numpy as jnp
 import numpy.testing as npt
+import pytest
 from absl.testing import parameterized
 from jax.scipy.special import logsumexp
 
@@ -20,6 +21,7 @@ class TestInverseCdf(chex.TestCase):
         self.Ms = [10, 100]
         self.Ns = [10, 100]
 
+    @pytest.mark.xdist_group(name="inverse_cdf_cpu")  # Serialize to avoid OOM
     @chex.all_variants(with_pmap=False, without_jit=False)
     @parameterized.parameters([0, 1, 2])
     def test_inverse_cdf(self, seed):
@@ -43,8 +45,9 @@ class TestInverseCdf(chex.TestCase):
 
             npt.assert_allclose(indices, expected)
 
+    @pytest.mark.xdist_group(name="inverse_cdf_cpu")  # Serialize to avoid OOM
     @chex.all_variants(with_pmap=False)
-    @parameterized.parameters([0, 1, 2, 3, 4])
+    @parameterized.parameters([0, 1, 2])
     def test_cpu_default_match(self, seed):
         key = jax.random.key(seed)
         for M, N in itertools.product(self.Ms, self.Ns):
