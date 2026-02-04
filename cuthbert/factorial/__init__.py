f47c78b573f4463b7422fbbd8d1b15b6a095817b
Sam Duffield <s@mduffield.com>
2026-02-04 15:03:20 +0000

    Update init

--- cuthbert/factorial/__init__.py
+++ cuthbert/factorial/__init__.py
@@ -1,2 +1,9 @@
 from cuthbert.factorial.filtering import filter
-from cuthbert.factorial.types import ExtractAndJoin, MarginalizeAndInsert
+from cuthbert.factorial.types import (
+    Factorializer,
+    GetFactorialIndices,
+    ExtractAndJoin,
+    MarginalizeAndInsert,
+)
+
+from cuthbert.factorial import gaussian
