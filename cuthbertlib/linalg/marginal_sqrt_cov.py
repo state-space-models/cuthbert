b3cf3597f8268e63992f80c4ff563ad64e33c397
Sam Duffield <s@mduffield.com>
2026-02-06 13:44:08 +0000

    Add back assertions

--- cuthbertlib/linalg/marginal_sqrt_cov.py
+++ cuthbertlib/linalg/marginal_sqrt_cov.py
@@ -23,5 +23,13 @@ def marginal_sqrt_cov(chol_cov: ArrayLike, start: int, end: int) -> Array:
         Lower triangular square root matrix of the marginal covariance matrix.
     """
     chol_cov = jnp.asarray(chol_cov)
+
+    assert chol_cov.ndim == 2, "chol_cov must be a 2D array"
+    assert chol_cov.shape[0] == chol_cov.shape[1], "chol_cov must be square"
+    assert start >= 0 and end <= chol_cov.shape[0], (
+        "start and end must be within the bounds of chol_cov"
+    )
+    assert start < end, "start must be less than end"
+
     chol_cov_select_rows = chol_cov[start:end, :]
     return tria(chol_cov_select_rows)
