c6453c38dee5acf2ea1cee973d90c97c598d8966
SamDuffield <34280297+SamDuffield@users.noreply.github.com>
2026-02-17 08:53:55 +0000

    Upload cuthbertlib to pypi (#190)
    * File structure without additional cuthbertlib folder
    
    * Change to hatchling
    
    * Add tied version and import in __init__
    
    * Sort imports
    
    * Infer cuthbert version rather than import from cuthbertlib
    
    * Update pypi.yaml

--- cuthbert/__init__.py
+++ cuthbert/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 from cuthbert import discrete, gaussian, smc
 from cuthbert.filtering import filter
 from cuthbert.inference import (
@@ -10,3 +12,7 @@ from cuthbert.inference import (
     SmootherPrepare,
 )
 from cuthbert.smoothing import smoother
+
+__version__ = version("cuthbert")  # Same version as cuthbertlib
+
+del version  # Remove so that cuthbert.version isn't a thing
