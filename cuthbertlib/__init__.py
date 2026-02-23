cc483c60c64fa4ce71e130b25428150d18609f20
Sam Duffield <s@mduffield.com>
2026-02-23 12:31:23 +0000

    Add cuthbertlib imports to init

--- cuthbertlib/__init__.py
+++ cuthbertlib/__init__.py
@@ -2,3 +2,14 @@ from importlib.metadata import version
 
 __version__ = version("cuthbertlib")
 del version
+
+from cuthbertlib import (
+    discrete,
+    kalman,
+    linalg,
+    linearize,
+    quadrature,
+    resampling,
+    smc,
+    stats,
+)
